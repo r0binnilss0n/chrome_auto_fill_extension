bc244cc6a69fbcca84d172fa8e8ba02ea2464af2
r0binnilss0n <nilsson.robin97@gmail.com>
2023-01-29 17:00:49 +0100

    added support for billable hours and internal com

--- popup.js
+++ popup.js
@@ -10,35 +10,45 @@ function fill_qbis_timelog() {
     console.log(keys);
 
     keys.forEach(key => {
-      if (key.startsWith("Man")) {
+      if (key.startsWith("Mon")) {
         to_fill["1"] = {
           arrive: copy_value[key]["start"],
           lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"]
+          leave: copy_value[key]["end"],
+          bill: copy_value[key]["bill"],
+          int: copy_value[key]["int"]
         };
-      } else if (key.startsWith("Tis")) {
+      } else if (key.startsWith("Tue")) {
         to_fill["11"] = {
           arrive: copy_value[key]["start"],
           lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"]
+          leave: copy_value[key]["end"],
+          bill: copy_value[key]["bill"],
+          int: copy_value[key]["int"]
         };
-      } else if (key.startsWith("Ons")) {
+      } else if (key.startsWith("Wed")) {
         to_fill["21"] = {
           arrive: copy_value[key]["start"],
           lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"]
+          leave: copy_value[key]["end"],
+          bill: copy_value[key]["bill"],
+          int: copy_value[key]["int"]
         };
-      } else if (key.startsWith("Tors")) {
+      } else if (key.startsWith("Thu")) {
         to_fill["31"] = {
           arrive: copy_value[key]["start"],
           lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"]
+          leave: copy_value[key]["end"],
+          bill: copy_value[key]["bill"],
+          int: copy_value[key]["int"]
         };
-      } else if (key.startsWith("Fre")) {
+      } else if (key.startsWith("Fri")) {
         to_fill["41"] = {
           arrive: copy_value[key]["start"],
           lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"]
+          leave: copy_value[key]["end"],
+          bill: copy_value[key]["bill"],
+          int: copy_value[key]["int"]
         };
       }
     });
@@ -75,6 +85,44 @@ function fill_qbis_timelog() {
   } catch (error) {
     console.error("Error in fill_with_data: ", error);
   }
+
+
+  try {
+    const to_fill_keys = Object.keys(to_fill);
+    console.log(to_fill);
+    console.log(to_fill_keys);
+
+    to_fill_keys.forEach(index => {
+      const day = to_fill[index];
+
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+          code: `
+            document.querySelectorAll('.phaseandactivity').forEach(el => {
+              if (el.textContent === "Billable hours") {
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day.bill}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
+              }
+              if (el.textContent === "Internal communication") {
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day.int}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
+              }
+            });
+          `
+        });
+      });
+    });
+  } catch (error) {
+    console.error("Error in fill_with_data: ", error);
+  }
+
+  //let tr = targetDiv.parentElement;
+
+  //document.querySelectorAll('.phaseandactivity')[0].closest("tr").querySelectorAll('input.k-input[tabindex="1"]')[1]
+
+
 }
 
 
@@ -95,8 +143,8 @@ function getClipboardText() {
   var insertionElement = document.activeElement; // start with the currently active element
   var nodeName = insertionElement.nodeName.toLowerCase(); // get the element type
   while (nodeName !== "body" && nodeName !== "div" && nodeName !== "li" && nodeName !== "th" && nodeName !== "td") { // if have not reached an element that it is valid to insert a div into (stopping eventually with 'body' if no others are found first)
-      insertionElement = insertionElement.parentNode; // go up the hierarchy
-      nodeName = insertionElement.nodeName.toLowerCase(); // get the element type
+    insertionElement = insertionElement.parentNode; // go up the hierarchy
+    nodeName = insertionElement.nodeName.toLowerCase(); // get the element type
   }
 
   // add element to document
@@ -116,7 +164,7 @@ function getClipboardText() {
   return clipboardText;
 }
 
-document.getElementById("AutoFillQbis").addEventListener("click", function() {
+document.getElementById("AutoFillQbis").addEventListener("click", function () {
   copy_value = JSON.parse(getClipboardText())
   console.log(copy_value)
   fill_qbis_timelog()
