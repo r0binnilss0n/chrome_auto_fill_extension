f6690c9ac0d9418c871918badb2c902bcf91dbdf
r0binnilss0n <nilsson.robin97@gmail.com>
2023-03-13 20:39:08 +0100

    cleanup

--- popup.js
+++ popup.js
@@ -1,65 +1,31 @@
-// let clipboardValue;
 let copy_value = ""
 
 function fill_qbis_timelog() {
   let to_fill = {};
-  console.log("in fill ");
 
+  // First i translate readable to id's to match qbis namings
   try {
     const keys = Object.keys(copy_value);
-    console.log(keys);
-
     keys.forEach(key => {
       if (key.startsWith("Mon")) {
-        to_fill["1"] = {
-          arrive: copy_value[key]["start"],
-          lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"],
-          bill: copy_value[key]["bill"],
-          int: copy_value[key]["int"]
-        };
+        to_fill["1"] = copy_value[key]
       } else if (key.startsWith("Tue")) {
-        to_fill["11"] = {
-          arrive: copy_value[key]["start"],
-          lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"],
-          bill: copy_value[key]["bill"],
-          int: copy_value[key]["int"]
-        };
+        to_fill["11"] = copy_value[key]
       } else if (key.startsWith("Wed")) {
-        to_fill["21"] = {
-          arrive: copy_value[key]["start"],
-          lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"],
-          bill: copy_value[key]["bill"],
-          int: copy_value[key]["int"]
-        };
+        to_fill["21"] = copy_value[key]
       } else if (key.startsWith("Thu")) {
-        to_fill["31"] = {
-          arrive: copy_value[key]["start"],
-          lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"],
-          bill: copy_value[key]["bill"],
-          int: copy_value[key]["int"]
-        };
+        to_fill["31"] = copy_value[key]
       } else if (key.startsWith("Fri")) {
-        to_fill["41"] = {
-          arrive: copy_value[key]["start"],
-          lunch: copy_value[key]["lunch"],
-          leave: copy_value[key]["end"],
-          bill: copy_value[key]["bill"],
-          int: copy_value[key]["int"]
-        };
+        to_fill["41"] = copy_value[key]
       }
     });
   } catch (error) {
     console.error("Error in fill_with_data: ", error);
   }
 
+  // Fill day data ( start, lunch, end)
   try {
     const to_fill_keys = Object.keys(to_fill);
-    console.log(to_fill);
-    console.log(to_fill_keys);
 
     to_fill_keys.forEach(index => {
       const day = to_fill[index];
@@ -67,15 +33,15 @@ function fill_qbis_timelog() {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(tabs[0].id, {
           code: `
-            document.querySelectorAll(\`input.Arrive[tabindex="${index}"]\`)[1].value = "${day.arrive}";
+            document.querySelectorAll(\`input.Arrive[tabindex="${index}"]\`)[1].value = "${day.START}";
             document.querySelectorAll(\`input.Arrive[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
             document.querySelectorAll(\`input.Arrive[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
 
-            document.querySelectorAll(\`input.Lunch[tabindex="${index}"]\`)[1].value = "${day.lunch}";
+            document.querySelectorAll(\`input.Lunch[tabindex="${index}"]\`)[1].value = "${day.LUNCH}";
             document.querySelectorAll(\`input.Lunch[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
             document.querySelectorAll(\`input.Lunch[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
 
-            document.querySelectorAll(\`input.Leave[tabindex="${index}"]\`)[1].value = "${day.leave}";
+            document.querySelectorAll(\`input.Leave[tabindex="${index}"]\`)[1].value = "${day.END}";
             document.querySelectorAll(\`input.Leave[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
             document.querySelectorAll(\`input.Leave[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
           `
@@ -86,11 +52,9 @@ function fill_qbis_timelog() {
     console.error("Error in fill_with_data: ", error);
   }
 
-
+  // Fill in our categories: billable and such
   try {
     const to_fill_keys = Object.keys(to_fill);
-    console.log(to_fill);
-    console.log(to_fill_keys);
 
     to_fill_keys.forEach(index => {
       const day = to_fill[index];
@@ -100,12 +64,47 @@ function fill_qbis_timelog() {
           code: `
             document.querySelectorAll('.phaseandactivity').forEach(el => {
               if (el.textContent === "Billable hours") {
-                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day.bill}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day["Billable hours"]}";
                 el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
                 el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
               }
               if (el.textContent === "Internal communication") {
-                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day.int}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day["Internal communication"]}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
+              }
+              if (el.textContent === "Business Development") {
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day["Business Development"]}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
+              }
+              if (el.textContent === "Competence development") {
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day["Competence development"]}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
+              }
+              if (el.textContent === "DevOps Internt") {
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day["DevOps intern"]}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
+              }
+              if (el.textContent === "Internal projects") {
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day["Internal projects"]}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
+              }
+              if (el.textContent === "Malvacom Board") {
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day["Malvacom Board"]}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
+              }
+              if (el.textContent === "Malvacom Web") {
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day["Malvacom Web"]}";
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
+              }
+              if (el.textContent === 'Miljöarbete') {
+                el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].value = "${day["Miljöarbete"]}";
                 el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("change"));
                 el.closest(\'tr\').querySelectorAll(\`input.k-input[tabindex="${index}"]\`)[1].dispatchEvent(new Event("blur"));
               }
@@ -117,12 +116,6 @@ function fill_qbis_timelog() {
   } catch (error) {
     console.error("Error in fill_with_data: ", error);
   }
-
-  //let tr = targetDiv.parentElement;
-
-  //document.querySelectorAll('.phaseandactivity')[0].closest("tr").querySelectorAll('input.k-input[tabindex="1"]')[1]
-
-
 }
 
 
@@ -166,8 +159,6 @@ function getClipboardText() {
 
 document.getElementById("AutoFillQbis").addEventListener("click", function () {
   copy_value = JSON.parse(getClipboardText())
-  console.log(copy_value)
   fill_qbis_timelog()
-
 });
 
